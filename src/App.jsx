98de84adb4449f824490358c495dbd609bf5c0b4
Geunone2 <rmsdnjsaos@gmail.com>
2025-04-21 20:10:19 +0900

    OrthographicCamera 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css';
-import PerspectiveCamera from "./component/cameras/PerspectiveCamera.js";
-import TwoPerspectiveCamera from "./component/cameras/TwoPerspectiveCameras.js";
-import TwentySphereCameras from "./component/cameras/TwentySphereCameras.js";
+import OrthographicCamera2 from "./component/cameras/OrthographicCamera2.js";
 
 function App() {
 
     return (
         <>
-            <TwentySphereCameras />
+            <OrthographicCamera2 />
         </>
     )
 }
