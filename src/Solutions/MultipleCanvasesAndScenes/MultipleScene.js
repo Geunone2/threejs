3f453dec30ebbb06bb826097fe6db09f2a6f0465
Geunwon Park <rmsdnjsaos@gmail.com>
2025-06-27 19:30:24 +0900

    MultipleScene 내용 수정

--- src/Solutions/MultipleCanvasesAndScenes/MultipleScene.js
+++ src/Solutions/MultipleCanvasesAndScenes/MultipleScene.js
@@ -1,114 +1,142 @@
 import * as THREE from "three";
+import {TrackballControls} from 'three/addons/controls/TrackballControls.js'
 
 export default function MultipleScene() {
 
-    const canvas = document.querySelector('#c');
-    const renderer = new THREE.WebGLRenderer({antialias: true, canvas})
+    const canvas = document.createElement('canvas');
+    const renderer = new THREE.WebGLRenderer({antialias: true, canvas, alpha: true});
+
+    renderer.setScissorTest(true);
 
     function makeScene(elem) {
         const scene = new THREE.Scene;
-        scene.background = new THREE.Color(0xffffff);
 
         const fov = 45;
         const aspect = 2;
         const near = 0.1;
         const far = 5;
         const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-        camera.position.z = 2;
         camera.position.set(0, 1, 2);
         camera.lookAt(0, 0, 0);
+        scene.add(camera);
+
+        const controls = new TrackballControls(camera, elem);
+        controls.noZoom = true;
+        controls.noPan = true;
 
         {
             const color = 0xFFFFFF;
-            const intensity = 1;
+            const intensity = 3;
             const light = new THREE.DirectionalLight(color, intensity);
             light.position.set(-1, 2, 4);
-            scene.add(light);
+            camera.add(light);
         }
 
-        return {scene, camera, elem};
+        return {scene, camera, controls};
     }
 
-    function setUpScene1() {
-        const sceneInfo = makeScene(document.querySelector('#box'));
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshPhongMaterial({color: 'red'});
-        const mesh = new THREE.Mesh(geometry, material);
-        sceneInfo.scene.add(mesh);
-        sceneInfo.mesh = mesh;
-        return sceneInfo;
+    const sceneElements = [];
+
+    function addScene(elem, fn) {
+        const ctx = document.createElement('canvas').getContext('2d');
+        elem.appendChild(ctx.canvas);
+        sceneElements.push({elem, ctx, fn});
     }
 
-    function setUpScene2() {
-        const sceneInfo = makeScene(document.querySelector('#pyramid'));
-        const radius = .8;
-        const widthSegments = 4;
-        const heightSegments = 2;
-        const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
-        const material = new THREE.MeshPhongMaterial({color: 'blue', flatShading: true});
-        const mesh = new THREE.Mesh(geometry, material);
-        sceneInfo.scene.add(mesh);
-        sceneInfo.mesh = mesh;
-        return sceneInfo;
-    }
+    const sceneInitFunctionsByName = {
+        'box': (elem) => {
+            const {scene, camera, controls} = makeScene(elem);
+            const geometry = new THREE.BoxGeometry(1, 1, 1);
+            const material = new THREE.MeshPhongMaterial({color: 'red'})
+            const mesh = new THREE.Mesh(geometry, material);
+            scene.add(mesh);
+            return (time, rect) => {
+                mesh.rotation.y = time * .1;
+                camera.aspect = rect.width / rect.height;
+                camera.updateProjectionMatrix();
+                controls.handleResize();
+                controls.update();
+                renderer.render(scene, camera);
+            };
+        },
 
-    const sceneInfo1 = setUpScene1();
-    const sceneInfo2 = setUpScene2();
+        'pyramid': (elem) => {
+            const {scene, camera, controls} = makeScene(elem);
+            const radius = .8;
+            const widthSegments = 4;
+            const heightSegments = 2;
+            const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+            const material = new THREE.MeshPhongMaterial({color: 'blue', flatShading: true});
+            const mesh = new THREE.Mesh(geometry, material);
+            scene.add(mesh);
+            return (time, rect) => {
+                mesh.rotation.y = time * .1;
+                camera.aspect = rect.width / rect.height;
+                camera.updateProjectionMatrix();
+                controls.handleResize();
+                controls.update();
+                renderer.render(scene, camera);
+            };
+        },
+    };
 
-    function renderSceneInfo(sceneInfo) {
-        const {scene, camera, elem} = sceneInfo;
+    document.querySelectorAll('[data-diagram]').forEach((elem) => {
+        const sceneName = elem.dataset.diagram;
+        const sceneInitFunction = sceneInitFunctionsByName[sceneName];
+        const sceneRenderFunction = sceneInitFunction(elem);
+        addScene(elem, sceneRenderFunction);
+    })
 
-        const {left, right, top, bottom, width, height} = elem.getBoundingClientRect();
 
-        const isOffscreen =
-            bottom < 0 ||
-            top > renderer.domElement.clientHeight ||
-            right < 0 ||
-            left > renderer.domElement.clientWidth;
+    function render( time ) {
 
-        if (isOffscreen) {
-            return;
-        }
-
-        camera.aspect = width / height;
-        camera.updateProjectionMatrix();
-
-        const positiveYUpBottom = renderer.domElement.clientHeight - bottom;
-        renderer.setScissor(left, positiveYUpBottom, width, height);
-        renderer.setViewport(left, positiveYUpBottom, width, height);
-
-        renderer.render(scene, camera)
-    }
-
-    function resizeRendererToDisplaySize(renderer) {
-        const canvas = renderer.domElement;
-        const width = canvas.clientWidth;
-        const height = canvas.clientHeight;
-        const needResize = canvas.width !== width || canvas.height !== height;
-        if (needResize) {
-            renderer.setSize(width, height, false);
-        }
-        return needResize;
-    }
-
-    function render(time) {
         time *= 0.001;
 
-        resizeRendererToDisplaySize(renderer);
+        for ( const { elem, fn, ctx } of sceneElements ) {
 
-        renderer.setScissorTest(false);
-        renderer.clear(true, true);
-        renderer.setScissorTest(true);
+            const rect = elem.getBoundingClientRect();
+            const { left, right, top, bottom, width, height } = rect;
+            const rendererCanvas = renderer.domElement;
 
-        sceneInfo1.mesh.rotation.y = time * .1;
-        sceneInfo2.mesh.rotation.y = time * .1;
+            const isOffscreen =
+                bottom < 0 ||
+                top > window.innerHeight ||
+                right < 0 ||
+                left > window.innerWidth;
 
-        renderSceneInfo(sceneInfo1);
-        renderSceneInfo(sceneInfo2);
+            if ( ! isOffscreen ) {
 
-        requestAnimationFrame(render);
+                if ( rendererCanvas.width < width || rendererCanvas.height < height ) {
+
+                    renderer.setSize( width, height, false );
+
+                }
+
+                if ( ctx.canvas.width !== width || ctx.canvas.height !== height ) {
+
+                    ctx.canvas.width = width;
+                    ctx.canvas.height = height;
+
+                }
+
+                renderer.setScissor( 0, 0, width, height );
+                renderer.setViewport( 0, 0, width, height );
+
+                fn( time, rect );
+
+                ctx.globalCompositeOperation = 'copy';
+                ctx.drawImage(
+                    rendererCanvas,
+                    0, rendererCanvas.height - height, width, height, // src rect
+                    0, 0, width, height ); // dst rect
+
+            }
+
+        }
+
+        requestAnimationFrame( render );
+
     }
 
     requestAnimationFrame(render);
 }
-
